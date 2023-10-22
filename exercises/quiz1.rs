6bb9019876ad259b132ae2b7fc0e3af8a27ab47c
NicoAsuka <NicoAsuka0423@gmail.com>
2023-10-22 18:08:23 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,20 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
+fn calculate_price_of_apples(count: i32) -> i32 {
+    let mut price = 0;
+    if count <= 40 {
+        price += count * 2;
+        price
+    } else {
+        price += count * 1;
+        price
+    }
+}
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
